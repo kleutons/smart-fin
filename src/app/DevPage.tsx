40db614ab3b5d74f23cb708c8a6ee6e2e9cb4360
Kleuton Novais <kleuton@msn.com>
2025-03-23 10:32:13 -0300

    upadate dev page

--- src/app/DevPage.tsx
+++ src/app/DevPage.tsx
@@ -1,3 +1,4 @@
+import ContactDev from "../components/ContactDev";
 import ContainerMain from "../components/ui/ContainerMain";
 import ContainerTop from "../components/ui/ContainerTop";
 import Header from "../components/ui/Header";
@@ -10,7 +11,27 @@ export default function DevPage(){
              <Header title='Desenvolvedor' />
         </ContainerTop>
         <ContainerMain>
-            Página do Desenvolvedor
+            <div className="flex flex-col gap-6 items-center mb-10">
+                <div className="w-[144px] h-[144px] flex items-center justify-center">
+                    <img src="https://avatars.githubusercontent.com/u/106082564?v=4" className="w-full h-full rounded-full object-cover object-cente"/>
+                </div>
+                <p>
+                    Olá! Sou Cleuton Novais, mais conhecido como Kleuton.  Sou um programador apaixonado por tecnologia, sempre em busca de expandir minhas habilidades e explorar novas soluções em constante evolução.
+                </p>
+            </div>
+
+            <h2 className="text-2xl font-bold mb-2">Projeto SmartFin</h2>
+            <p className="mb-4">
+                Este aplicativo é uma poderosa ferramenta para auxiliar os usuários no controle financeiro, proporcionando maior organização e clareza sobre a saúde financeira de empresas ou indivíduos. Com ele, o gerenciamento de gastos mensais se torna mais fácil e eficiente, promovendo decisões financeiras mais conscientes.
+            </p>
+            <h3 className="text-xl font-semibold mb-2">Tecnologias Utilizadas:</h3>
+            <ul className="list-disc list-inside mb-4">
+                <li><strong>FrontEnd:</strong> React, JavaScript com TypeScript e TailwindCSS</li>
+                <li><strong>Banco de Dados:</strong> IndexDB</li>
+            </ul>
+            <h3 className="text-xl font-semibold mb-2">Contato / Links:</h3>
+
+            <ContactDev />
         </ContainerMain>
         </>
     )
